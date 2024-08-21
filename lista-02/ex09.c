eb01a371ba1461b040a895cb3488430059f9271c
Vinicio :) <vinicioolivindo92@gmail.com>
2024-08-21 11:46:01 -0300

    .

--- lista-02/ex09.c
+++ lista-02/ex09.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main()
-{
-	char str[50];
-	int i = 0;
-	fgets(str, sizeof(str), stdin);
+int main() {
+    char str[51]; 
+    
+    while (fgets(str, sizeof(str), stdin) != NULL) {
+        int i = 0;
+        int flag = 0; 
+        
+        while (str[i] != '\0') {
+            if (str[i] != ' ') {
+                if (flag % 2 == 0) {
+                    str[i] = toupper(str[i]);
+                } else {
+                    str[i] = tolower(str[i]);
+                }
+                flag++;
+            }
+            i++;
+        }
+        if (str[0] != ' ' && isalpha(str[0])) {
+            str[0] = toupper(str[0]);
+        }
+        printf("%s", str);
+    }
 
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ')
-		{
-			if (i % 2 == 0)
-			{
-				str[i] = toupper(str[i]);
-			}
-		}
-		i++;
-	}
-	printf("%s", str);
-	return 0;
+    return 0;
 }
