c72a6aa6660f5cb83f2095d15f8c3a4b285d96c9
Vinicio :) <vinicioolivindo92@gmail.com>
2024-08-23 13:54:38 -0300

    att

--- qst41.c
+++ qst41.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#define caracteres 21
 
 int main()
 {
-    char string01[20];
-    scanf("%s", string01);
-    
-    printf("%s", string01[1]);
+    char string01[caracteres], string02[caracteres];
+
+    fgets(string01, sizeof(string01), stdin);
+    fgets(string02, sizeof(string02), stdin);
+
+
+    printf("A primeira string lida foi: %s\n E sua segunda letra eh: %c \n",string01, string01[1]);
+    printf("A segunda string lida foi: %s\n E sua segunda letra eh: %c",string02, string02[1]);
 }
